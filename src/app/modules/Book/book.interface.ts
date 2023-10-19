9fb7e5d9c27c1a4965d0c26fccd6780c2b5c4ec9
arju10 <arjuarju71@gmail.com>
2023-10-19 18:22:17 +0600

    create book interface

---
+++ src/app/modules/Book/book.interface.ts
@@ -0,0 +1,19 @@
+import { Document } from "mongoose";
+
+export interface IProduct extends Document {
+  title: string;
+  author: string[];
+  genre: string;
+  publicationYear: number;
+  publisher: {
+    name: string;
+    location: string;
+  };
+  description: string;
+  reviews?: {
+    user: string;
+    comment: string;
+  }[];
+  rating: number;
+  price: string;
+}
